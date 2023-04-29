a1cec5fedda6d8c4d89d628c3c98ba4e3266ad5c
Samson Mulugeta <smulug1244@gmail.com>
2023-04-29 19:48:49 -0400

    format changes

--- pokemon.py
+++ pokemon.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 
 class Pokemon:
+    #Works
     def __init__(self, data):
         self.id = data['id']
         self.name = data['name']
@@ -16,17 +17,20 @@ class Pokemon:
         self.speed = data['base']['Speed']
 
 class Pokedex:
+    #Works
     def __init__(self, file_path):
         with open(file_path, 'r', encoding='utf-8') as f:
             pokemon_data = json.load(f)
         self.pokemon = [Pokemon(data) for data in pokemon_data]
-    
+        
+    #Works
     def search_by_name(self, name):
         for pkmn in self.pokemon:
             if pkmn.name['english'].lower() == name.lower():
                 return pkmn
         return None
     
+    #Works
     def search_by_type(self, p_type, limit):
         matching_pokemon = []
         for pkmn in self.pokemon:
@@ -34,7 +38,7 @@ class Pokedex:
                 matching_pokemon.append(pkmn)
         random.shuffle(matching_pokemon)
         return random.sample(matching_pokemon, min(limit, len(matching_pokemon)))
- 
+    #Works
     def compare_pokemon(self, pokemon1, pokemon2):
         print(f"Comparing {pokemon1.name['english']} and {pokemon2.name['english']}...\n")
         stats_to_compare = ['hp', 'attack', 'defense', 'sp_attack', 'sp_defense', 'speed']
@@ -45,6 +49,24 @@ class Pokedex:
                 print(f"{pokemon2.name['english']} has higher {stat}: {getattr(pokemon2, stat)} vs {getattr(pokemon1, stat)}")
             else:
                 print(f"{pokemon1.name['english']} and {pokemon2.name['english']} have the same {stat}: {getattr(pokemon1, stat)}")
+    #Works
+    def pokemon_visualize(self, name):
+        pokedex_df = pd.read_csv("pokedex.csv")
+        pokemon = pokedex.search_by_name(name)
+        if not pokemon:
+            print('Pokemon not found.')
+            return
+        pokemon_df = pokedex_df.loc[pokedex_df["name/english"] == name]
+        cols = ["base/HP", "base/Attack", "base/Defense", "base/Sp. Attack", "base/Sp. Defense", "base/Speed"]
+        attribute_values = pokemon_df[cols].values[0].tolist()
+        figure, ax = plt.subplots(figsize=(8, 6))
+        ax.bar(cols, attribute_values)
+        ax.set_xlabel("Attribute")
+        ax.set_ylabel("Value")
+        ax.set_title(pokemon.name["english"])
+        for i, v in enumerate(attribute_values):
+            ax.text(i, v+1, str(v), ha='center', fontsize=10)
+        plt.show()
     
     def filter_by_hp(self, min_hp=None, max_hp=None):
         filtered_pokemon = []
@@ -137,25 +159,6 @@ class Pokedex:
         all_types = ['Normal', 'Fire', 'Water', 'Grass', 'Electric', 'Ice', 'Fighting', 'Poison', 'Ground', 'Flying', 'Psychic', 'Bug', 'Rock', 'Ghost', 'Dragon', 'Dark', 'Steel', 'Fairy']
         print('All types:', ', '.join(all_types))
         
-    #Works
-    def pokemon_visualize(self, name):
-        pokedex_df = pd.read_csv("pokedex.csv")
-        pokemon = pokedex.search_by_name(name)
-        if not pokemon:
-            print('Pokemon not found.')
-            return
-        pokemon_df = pokedex_df.loc[pokedex_df["name/english"] == name]
-        cols = ["base/HP", "base/Attack", "base/Defense", "base/Sp. Attack", "base/Sp. Defense", "base/Speed"]
-        attribute_values = pokemon_df[cols].values[0].tolist()
-        figure, ax = plt.subplots(figsize=(8, 6))
-        ax.bar(cols, attribute_values)
-        ax.set_xlabel("Attribute")
-        ax.set_ylabel("Value")
-        ax.set_title(pokemon.name["english"])
-        for i, v in enumerate(attribute_values):
-            ax.text(i, v+1, str(v), ha='center', fontsize=10)
-        plt.show()
-
 if __name__ == "__main__":
     # Create a Pokedex object
     pokedex = Pokedex('pokedex.json')
